c92b1428069b08581dffc0c6cc50dd87565d67cf
heshanxiu <nanyamashuu@heshanxiudeMacBook-Pro.local>
2020-03-29 04:40:00 -0700

    extract trimmed data with labels

--- covid19_data.py
+++ covid19_data.py
@@ -37,7 +37,7 @@ SPLIT2NAME = {
     'test': 'test.json',
 }
 
-time = "1/22/20,1/23/20,1/24/20,1/25/20,1/26/20,1/27/20,1/28/20,1/29/20,1/30/20,1/31/20,2/1/20,2/2/20,2/3/20,2/4/20,2/5/20,2/6/20,2/7/20,2/8/20,2/9/20,2/10/20,2/11/20,2/12/20,2/13/20,2/14/20,2/15/20,2/16/20,2/17/20,2/18/20,2/19/20,2/20/20,2/21/20,2/22/20,2/23/20,2/24/20,2/25/20,2/26/20,2/27/20,2/28/20,2/29/20,3/1/20,3/2/20,3/3/20,3/4/20,3/5/20,3/6/20,3/7/20,3/8/20,3/9/20,3/10/20,3/11/20,3/12/20,3/13/20,3/14/20,3/15/20,3/16/20,3/17/20,3/18/20,3/19/20,3/20/20,3/21/20,3/22/20,3/23/20"
+time = "1/22/20,1/23/20,1/24/20,1/25/20,1/26/20,1/27/20,1/28/20,1/29/20,1/30/20,1/31/20,2/1/20,2/2/20,2/3/20,2/4/20,2/5/20,2/6/20,2/7/20,2/8/20,2/9/20,2/10/20,2/11/20,2/12/20,2/13/20,2/14/20,2/15/20,2/16/20,2/17/20,2/18/20,2/19/20,2/20/20,2/21/20,2/22/20,2/23/20,2/24/20,2/25/20,2/26/20,2/27/20,2/28/20,2/29/20,3/1/20,3/2/20,3/3/20,3/4/20,3/5/20,3/6/20,3/7/20,3/8/20,3/9/20,3/10/20,3/11/20,3/12/20,3/13/20,3/14/20,3/15/20,3/16/20,3/17/20,3/18/20,3/19/20,3/20/20"
 
 
 """
@@ -46,7 +46,7 @@ time_series_column = [Province/State,Country/Region,Lat,Long,1/22/20,1/23/20,1/2
 2/9/20,2/10/20,2/11/20,2/12/20,2/13/20,2/14/20,2/15/20,2/16/20,2/17/20,2/18/20,2/19/20,2/20/20,
 2/21/20,2/22/20,2/23/20,2/24/20,2/25/20,2/26/20,2/27/20,2/28/20,2/29/20,3/1/20,3/2/20,3/3/20,
 3/4/20,3/5/20,3/6/20,3/7/20,3/8/20,3/9/20,3/10/20,3/11/20,3/12/20,3/13/20,3/14/20,3/15/20,3/16/20,
-3/17/20,3/18/20,3/19/20,3/20/20,3/21/20,3/22/20,3/23/20]
+3/17/20,3/18/20,3/19/20,3/20/20]
 
 covid19_country_column = Country,Region,Population,Area (sq. mi.),Population Density,GDP ($per capita),Literacy (%),1/22/20,
 
@@ -54,6 +54,16 @@ covid19_country_column = Country,Region,Population,Area (sq. mi.),Population Den
 """
 
 DAY_OF_OBSERVATION = 14
+
+def target(mode, Country, time):
+    if mode == "Country":
+        df = pd.read_csv(COUNTRY_DATA, index_col='Country')
+        result = df[time][Country]
+    elif mode == "Region":
+        df = pd.read_csv(REGION_DATE, index_col='ID')
+        result = df[time][Country]
+
+    return result if not np.isnan(result) else -1
 
 #county, province, country, 1/22/20
 def new_confirmed(mode, County, Province, Country, time):
@@ -112,7 +122,6 @@ def new_confirmed(mode, County, Province, Country, time):
             cumulative.append(float(df[date][Country]))
             if i > 0:
                 increase.append(cumulative[i] - cumulative[i-1])
-
     # start_num, day1 increase, ..., day 14 increase
     #
     # print(torch.FloatTensor(increase))
@@ -155,30 +164,15 @@ class Covid19Dataset(Dataset):
         #
         # train_set, test_set = train_test_split(entire_country, test_size = 0.1, random_state = 42)
         # train_set, dev_set = train_test_split(train_set, test_size=0.1, random_state=42)
-        #
-        if splits == "train":
-            self.id_to_output = json.load(open(os.path.join(ROOT+"train/", "id_to_output.json")))
-        if splits == "dev":
-            self.id_to_output = json.load(open(os.path.join(ROOT+"dev/", "id_to_output.json")))
-        if splits == "test":
-            self.id_to_output = json.load(open(os.path.join(ROOT+"test/", "id_to_output.json")))
 
-        # self.date = time.split(",")[DAY_OF_OBSERVATION:]
-        #
-        # # {"location, date", id}
-        # # {id: (location, population, policy), (day1, day2, ..., day14)}
-        # self.input_to_id = {}
-        # self.id_to_output = {}
-        #
-        # for i, curr_location in enumerate(self.location):
-        #     for j, date in enumerate(self.date):
-        #         self.input_to_id[(curr_location, date)] = len(self.input_to_id)
-        #         _id = self.input_to_id[(curr_location, date)]
-        #
-        #         city = city_info("", curr_location)
-        #         cumulative_array = new_confirmed("", "", curr_location, date)
-        #         self.id_to_output[_id] = (city, cumulative_array)
 
+        id_output_path = os.path.join(ROOT+'trimmed_data/'+splits+'/', "id_to_output.json")
+        input_id_path = os.path.join(ROOT+'trimmed_data/'+splits+'/', "input_to_id.json")
+        id_target_path = os.path.join(ROOT+'trimmed_data/'+splits+'/', "target.json")
+
+        self.id_to_output = json.load(open(id_output_path))
+        self.input_to_id = json.load(open(input_id_path))
+        self.target = json.load(open(id_target_path))
 
         print("Use %d data in dataset" % (len(self.id_to_output)))
         print()
@@ -190,8 +184,9 @@ class Covid19Dataset(Dataset):
 
         # Get image info
         city, covid = self.id_to_output[str(item)]
+        target = self.target[str(item)]
 
-        return torch.FloatTensor(city), torch.FloatTensor(covid) if city is not None else None, torch.FloatTensor(covid)
+        return torch.FloatTensor(city), torch.FloatTensor(covid), torch.FloatTensor(target)
 
     def city_features(self):
         return 5
--- extract.py
+++ extract.py
@@ -11,7 +11,7 @@ import csv
 from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
 from sklearn.model_selection import train_test_split
-from covid19_data import new_confirmed, city_info, time
+from covid19_data import new_confirmed, city_info, time, target
 
 ROOT = '/local/shanxiu/ubert/covid/'
 ARCHIVED_COVID_DATA = os.path.join(ROOT, 'archived_data/archived_time_series/time_series_19-covid-Confirmed_archived_0325.csv')
@@ -25,8 +25,11 @@ if __name__ == "__main__":
     entire_country = pd.read_csv(COUNTRY_DATA)['Country'].values.tolist()
     entire_region = pd.read_csv(REGION_DATA)['ID'].values.tolist()
 
-    country_train, country_test, region_train, region_test = train_test_split(entire_country, entire_region, test_size=0.1, random_state=42)
-    country_train, country_dev, region_train, region_dev = train_test_split(country_train, region_train, test_size=0.1, random_state=42)
+    country_train, country_test = train_test_split(entire_country, test_size=0.1, random_state=42)
+    country_train, country_dev = train_test_split(country_train,  test_size=0.1, random_state=42)
+
+    region_train, region_test = train_test_split(entire_region, test_size=0.1, random_state=42)
+    region_train, region_dev = train_test_split(region_train, test_size=0.1, random_state=42)
 
     list = [country_train, country_dev, country_test, region_train, region_dev, region_test]
     name = ["train", "dev", "test"]
@@ -34,30 +37,56 @@ if __name__ == "__main__":
     date_list = time.split(",")[DAY_OF_OBSERVATION:]
     input_to_id = {}
     id_to_output = {}
+    id_to_target = {}
 
     for index, split in enumerate(name):
         country_data = list[index]
         county_data = list[index+3]
         for i, curr_location in enumerate(country_data):
+            country_all_zero = False
             for j, date in enumerate(date_list):
-                loc_date = curr_location + '!' + date
-                _id = len(input_to_id)
-                input_to_id[loc_date] = _id
 
+                loc_date = curr_location + '!' + date
                 city = city_info("Country","", curr_location)
                 cumulative_array = new_confirmed("Country", "", "", curr_location, date)
-                id_to_output[_id] = (city, cumulative_array)
+                goal = target("Country", curr_location, date)
+
+                if cumulative_array[-1] != 0 and goal > 0:
+                    _id = len(input_to_id)
+                    input_to_id[loc_date] = _id
+                    id_to_target[_id] = goal
+                    id_to_output[_id] = (city, cumulative_array)
+                    country_all_zero = True
+                if j == len(date_list) - 1 and country_all_zero is True:
+                    _id = len(input_to_id)
+                    input_to_id[loc_date] = _id
+                    id_to_target[_id] = 0
+                    id_to_output[_id] = (city, [0 for i in range(15)])
+
 
         for i, curr_location in enumerate(county_data):
+            city_all_zero = False
             for j, date in enumerate(date_list):
                 loc_date = curr_location + '!' + date
-                _id = len(input_to_id)
-                input_to_id[loc_date] = _id
 
                 city = city_info("Region","", curr_location)
-                cumulative_array = new_confirmed("Region", "", "", curr_location)
-                id_to_output[_id] = (city, cumulative_array)
+                cumulative_array = new_confirmed("Region", "", "", curr_location, date)
+                goal = target("Region", curr_location, date)
 
-        outfile = os.path.join(ROOT, (name[index]))
+                if cumulative_array[-1] != 0 and goal > 0:
+                    _id = len(input_to_id)
+                    input_to_id[loc_date] = _id
+                    id_to_target[_id] = goal
+                    id_to_output[_id] = (city, cumulative_array)
+                    city_all_zero = True
+
+                if j == len(date_list) - 1 and city_all_zero is True:
+                    _id = len(input_to_id)
+                    input_to_id[loc_date] = _id
+                    id_to_target[_id] = 0
+                    id_to_output[_id] = (city, [0 for i in range(15)])
+
+
+        outfile = os.path.join(ROOT+'trimmed_data/', (name[index]))
         json.dump(input_to_id, open(os.path.join(outfile, "input_to_id.json"), "w"))
         json.dump(id_to_output, open(os.path.join(outfile, "id_to_output.json"), "w"))
